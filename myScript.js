1e0cb5ef2ec5e64283f76527a5feb62e46c1f6af
GlendleNguyen <glendle.nguyen@gmail.com>
2021-12-09 18:05:54 +0800

    Add textoutput for result

--- myScript.js
+++ myScript.js
@@ -10,47 +10,58 @@ function computerPlay() {
 
 function playRound(playerChoice) {
     let computerChoice = computerPlay()
+    let textoutput = ""
 
     switch (playerChoice) {
         case 'water':
             if (computerChoice === 'grass') {
                 computerWins += 1
+                textoutput += "Bot (grass) beats You (water)"
                 break
             }
             else if (computerChoice === 'fire') {
                 playerWins += 1
+                textoutput += "You (water) beat Bot (fire)"
                 break
             }
             else {
+                textoutput += "It's a draw!"
                 break
             }
 
         case 'fire':
             if (computerChoice === 'water') {
                 computerWins += 1
+                textoutput += "Bot (water) beats You (fire)"
                 break
             }
             else if (computerChoice === 'grass') {
                 playerWins += 1
+                textoutput += "You (fire) beat Bot (grass)"
                 break
             }
             else {
+                textoutput += "It's a draw!"
                 break
             }
 
         case 'grass':
             if (computerChoice === 'fire') {
                 computerWins += 1
+                textoutput += "Bot (fire) beats You (grass)"
                 break
             }
             else if (computerChoice === 'water') {
                 playerWins += 1
+                textoutput += "You (grass) beat Bot (water)"
                 break
             }
             else {
+                textoutput += "It's a draw!"
                 break
             }
     }
+    document.getElementById('textbox').innerHTML = textoutput
     return
 }
 
